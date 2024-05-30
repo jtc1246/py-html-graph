8f6ed68d0b6e4209a5ada7b419588e3726d22c45
jtc1246 <jtc1246@outlook.com>
2024-05-30 07:06:57 -0400

    makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	python3 server.py make
+
+clean:
+	rm -r ./html/index.html
--- server.py
+++ server.py
@@ -4,12 +4,43 @@ from _thread import start_new_thread
 from time import sleep
 
 from myBasics import strToBase64
+import sys
+import os
+
+if(len(sys.argv)>=2 and sys.argv[1] == 'make'):
+    with open('./html/chartjs_with_move.html', 'r') as f:
+        html = f.read()
+    with open('./html/chart.js', 'r') as f:
+        chartjs = f.read()
+    with open('./html/chartjs_with_move.js', 'r') as f:
+        js = f.read()
+    with open('./html/reset.css', 'r') as f:
+        reset = f.read()
+    with open('./html/chartjs_with_move.css', 'r') as f:
+        css = f.read()
+    with open('./html/style.js', 'r') as f:
+        stylejs = f.read()
+    with open('./html/axis.js', 'r') as f:
+        axisjs = f.read()
+    with open('./html/data_worker.js', 'r') as f:
+        worker = f.read()
+    worker = strToBase64(worker)
+    html = html.replace('<script src="chart.js"></script>', f'<script>{chartjs}</script>')
+    html = html.replace('<script src="chartjs_with_move.js"></script>', f'<script>{js}</script>')
+    html = html.replace('<link rel="stylesheet" href="reset.css">', f'<style>{reset}</style>')
+    html = html.replace('<link rel="stylesheet" href="chartjs_with_move.css">', f'<style>{css}</style>')
+    html = html.replace('<script src="style.js"></script>', f'<script>{stylejs}</script>')
+    html = html.replace('<script src="axis.js"></script>', f'<script>{axisjs}</script>')
+    html = html.replace("'$workerb64$'", f"'{worker}'")
+    with open('./html/index.html', 'w') as f:
+        f.write(html)
+    os._exit(0)
 
 
 class Request(BaseHTTPRequestHandler):
     def do_GET(self):
         path = self.path
-        if (path not in ['/', '/index.html', '/data_worker.js']):
+        if (path not in ['/', '/index.html']):
             print(404)
             self.send_response(404)
             self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')

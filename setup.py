8bfd6bf6295d951529f245214876bf3c64d04a4d
jtc1246 <jtc1246@outlook.com>
2024-07-19 01:07:14 +0800

    update gif

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='py-html-graph',
-    version='0.1.0',
+    version='0.1.1',
     author='Tiancheng Jiao',
     author_email='jtc1246@outlook.com',
     url='https://github.com/jtc1246/py-html-graph',

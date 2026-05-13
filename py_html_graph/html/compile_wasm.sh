7ce12401209e41200c383eb9449e01d29e161e0d
jtc <jtc@histrularity.com>
2026-05-13 19:39:17 +0000

    update wasm to O1

--- py_html_graph/html/compile_wasm.sh
+++ py_html_graph/html/compile_wasm.sh
@@ -5,7 +5,7 @@ cd "$(readlink -f "$(dirname "$0")")"
 source ~/.bashrc
 conda activate wasm
 EXPORTS="['_init','_get_max_level','_get_max_cache_all_level','_get_io_buffer','_alloc_whole_cache','_has_whole_cache','_access_data','_access_from_cache','_store_miss_response','_set_mouse_pos','_update_cache_ranges','_get_pending_ptr','_alloc_temp_input','_write_preload_response','_mark_requesting','_check_retry','_get_retry_info','_retry_mark','_get_has_request','_get_current_level','_clear_request']"
-emcc cache_wasm.cpp -O0 --no-entry \
+emcc cache_wasm.cpp -O1 --no-entry \
     -s EXPORTED_FUNCTIONS="$EXPORTS" \
     -s ALLOW_MEMORY_GROWTH=1 \
     -o cache_wasm.wasm
